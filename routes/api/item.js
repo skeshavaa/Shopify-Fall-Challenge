f93639df708267022c91c3d3d8365def4fa4e4f8
skeshavaa <s.keshavaa@gmail.com>
2021-04-28 15:20:15 -0400

    image now getting labels from google vision

--- routes/api/item.js
+++ routes/api/item.js
@@ -1,18 +1,27 @@
 const express = require('express')
-const AWS = require('aws-sdk');
 const router = express.Router();
 const auth = require('../../middleware/auth')
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
 var upload = require('../api/file-upload')
+const vision = require('@google-cloud/vision')
+
 
 const singleImage = upload.single('image')
+const client = new vision.ImageAnnotatorClient({
+    keyFilename: 'vision-auth.json'
+});
 
-router.post('/addImage', auth, (req, res) => {
+router.post('/addImage', auth, async (req, res) => {
+    var url;
+    singleImage(req, res, function () {
+        url = req.file.location
+        console.log(req.file.location)
+        client.labelDetection(req.file.location).then(results => {
+            const labels = results[0].labelAnnotations;
 
-   singleImage(req, res, function() {
-       return res.json({imageurl: req.file.location})
-   })
+            labels.forEach((label) => console.log(label))
+        }).catch(err => console.log(err))
+        return res.json({ imageurl: req.file.location })
+    })
 })
 
 
