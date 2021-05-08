9f10ddec6b3b1989dd792baa121499f72e09136f
skeshavaa <s.keshavaa@gmail.com>
2021-05-08 19:15:06 -0400

    UI fixes and additions

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
     return (
         <div className="navContainer">
             <div className="leftContainer">
-                <h1 className="navLink">Postman Request Library</h1>
+                <a className="navA" href="api-docs" target="_blank"><h1 className="navLink">Postman Request Library</h1></a>
                 <h1 className="navLink">Swagger Documentation</h1>
             </div>
             {state.isAuthenticated ? 
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../styles/Landing.css'
+import Github from '../githubIcon.png'
 import Search from '../components/Search.jsx'
 
 function Landing() {
     return (
         <div className="landingContainer">
-            <h1 className="heading1">Keshavaa's Image Repository</h1>
+            <div className="row">
+                <h1 className="heading1">Keshavaa's Image Repository</h1>
+                <a target="_blank" href="https://github.com/skeshavaa/Shopify-Fall-Challenge"><img className="githubIcon" src={Github}/></a>
+            </div>
             <h1 className="heading2">Submission for Shopfy’s Fall Intern Challenge</h1>
+            <a className="APILink" href="/api-docs" target="_blank"><h1 className="heading3">If you would like to add an image, please use the API <span className="highlight">Located Here!</span></h1></a>
             <Search />
         </div>
     )
